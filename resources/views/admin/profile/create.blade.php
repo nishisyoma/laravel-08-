87d12565b23bb8bc4d75dc7f8c685d59e8fece2e
西　翔駿 <syouma24free@gmail.com>
2021-03-05 08:08:35 +0000

    課題11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,16 +1,15 @@
-
 {{-- layouts/admin.bland.phpを読み込む --}}
-    @extends('layouts.admin')
+    @extends('layouts.profile')
     
     {{-- admin.bland.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-    @section('title','ニュースの新規作成')
+    @section('title','Myプロフィール')
     
     {{-- admin.bland.phpの@yield('content')に以下のタグを埋め込む --}}
     @section('content')
         <div class="container">
             <div class="row">
                 <div class="col-md-8 mx-auto">
-                    <h2>ニュース新規作成</h2>
+                    <h2>Myプロフィール</h2>
                 </div>
             </div>
         </div>
